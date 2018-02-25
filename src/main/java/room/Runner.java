aa7967f36210e4046e3ec8a0f1f3924b58fcfcbd
Pakman198469 <o>
2018-02-25 11:55:48 +0300

    test

---
+++ src/main/java/room/Runner.java
@@ -0,0 +1,10 @@
+package room;
+
+/**
+ * Created by Aleh_Hutyrchyk on 2/23/2018.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Home");
+    }
+}
